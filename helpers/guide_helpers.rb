846c9864291c41c9ef83d9d21c8dc7d3dd0aa6ed
Tosainu <tosainu.maple@gmail.com>
2015-03-10 09:46:45 +0900

    add page_title helper

---
+++ helpers/guide_helpers.rb
@@ -0,0 +1,13 @@
+module GuideHelpers
+  def page_title
+    title = ""
+    if current_page.data.title
+      title << current_page.data.title
+      if yield_content(:title)
+        title << ' ' << yield_content(:title)
+      end
+      title << ' | '
+    end
+    title << data.site.title
+  end
+end

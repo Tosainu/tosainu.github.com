3b18b139f259d546f2ba011bcc987b71fd3dbcdc
Tosainu <tosainu.maple@gmail.com>
2016-07-02 15:04:37 +0900

    Refactor entry lists page and title

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -1,13 +1,41 @@
 module CustomHelpers
   def page_title
     title = ""
-    if current_page.data.title
-      title << current_page.data.title
-      if yield_content(:title)
-        title << ' ' << yield_content(:title)
+    if is_blog_article?
+      title << current_article.title << ' | '
+    elsif current_page.data.title
+      title << current_page.data.title << ' | '
+    elsif page_metadata['page_type']
+      case page_metadata['page_type']
+      when 'tag'
+        title << 'Tag: ' << page_metadata['tagname'] << ' | '
+      when 'year'
+        title << formatted_year << ' | '
+      when 'month'
+        title << formatted_year_month << ' | '
+      when 'day'
+        title << formatted_year_month_day << ' | '
       end
-      title << ' | '
     end
     title << data.site.title
+  end
+
+  def page_metadata
+    current_resource.metadata[:locals]
+  end
+
+  def formatted_year
+    Date.new(page_metadata['year']).strftime('%Y')
+  end
+
+  def formatted_year_month
+    Date.new(page_metadata['year'],
+             page_metadata['month']).strftime('%B %Y')
+  end
+
+  def formatted_year_month_day
+    Date.new(page_metadata['year'],
+             page_metadata['month'],
+             page_metadata['day']).strftime('%e. %B %Y')
   end
 end

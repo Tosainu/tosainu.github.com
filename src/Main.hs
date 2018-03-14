22d97f4bf3f19a9b52668719f42d9c0dee0570f5
Kenta Sato <tosainu.maple@gmail.com>
2018-03-14 13:12:51 +0900

    Add initial hakyll rules

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import           Hakyll
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = hakyllWith hakyllConfig $ do
+  match ("CNAME" .||. "favicon.ico" .||. "images/**") $ do
+    route idRoute
+    compile copyFileCompiler
+
+  -- "entry/year/month/day/title/index.md"
+  match "entry/*/*/*/*/index.md" $ do
+    route $ setExtension "html"
+    compile pandocCompiler
+
+  match "entry/*/*/*/*/**" $ do
+    route idRoute
+    compile copyFileCompiler
+
+--- Configurations
+hakyllConfig :: Configuration
+hakyllConfig = defaultConfiguration
+  { destinationDirectory = "build"
+  , storeDirectory       = ".cache"
+  , tmpDirectory         = ".cache/tmp"
+  , previewHost          = "0.0.0.0"
+  , previewPort          = 4567
+  }
+

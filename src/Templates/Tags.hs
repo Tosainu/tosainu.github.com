edc9e985e3641b46a6a5bc6771577aeae3b8d165
Kenta Sato <tosainu.maple@gmail.com>
2018-03-18 03:05:21 +0900

    Use Lucid instead of Blaze

--- src/Templates/Tags.hs
+++ src/Templates/Tags.hs
@@ -3,20 +3,27 @@ module Templates.Tags
   , allTagsListField
   ) where
 
+import           Control.Monad
+import           Data.Maybe
+import qualified Data.Text      as T
+import qualified Data.Text.Lazy as TL
 import           Hakyll
-import           Text.Blaze.Html                 ((!))
-import qualified Text.Blaze.Html.Renderer.String as B
-import qualified Text.Blaze.Html5                as BH
-import qualified Text.Blaze.Html5.Attributes     as BA
+import           Lucid.Base
+import           Lucid.Html5
 
 tagsListField :: String -> Tags -> Context a
-tagsListField = tagsFieldWith getTags toLink' $ mconcat . map BH.li
+tagsListField key tags = field key $ \item -> do
+  tags' <- getTags $ itemIdentifier item
+  links <- forM tags' $ \tag -> do
+    route' <- getRoute $ tagsMakeId tags tag
+    return $ toLink' tag route'
+  return $ TL.unpack $ renderText $ mconcat $ map li_ $ catMaybes links
   where toLink' _   Nothing     = Nothing
         toLink' tag (Just path) = Just $ toLink tag path
 
 allTagsListField :: String -> Tags -> Context a
 allTagsListField key tags = field key $ \_ -> renderTags toLink' concat tags
-  where toLink' tag path _ _ _ = B.renderHtml $ BH.li $ toLink tag path
+  where toLink' tag path _ _ _ = TL.unpack $ renderText $ li_ $ toLink tag path
 
-toLink :: String -> String -> BH.Html
-toLink text path = BH.a ! BA.href (BH.toValue $ toUrl path) $ BH.span $ BH.toHtml text
+toLink :: String -> String -> Html ()
+toLink text path = a_ [href_ (T.pack $ toUrl path)] $ span_ $ toHtml text

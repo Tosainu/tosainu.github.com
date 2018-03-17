43ac6e1587e18c510f9a8b8e2205e5202cdf1d5b
Kenta Sato <tosainu.maple@gmail.com>
2018-03-17 14:18:36 +0900

    Add navigation bar

--- src/Templates/EntryList.hs
+++ src/Templates/EntryList.hs
@@ -3,6 +3,8 @@
 module Templates.EntryList where
 
 import           Control.Monad
+import           Data.Maybe
+import qualified Data.Text             as T
 import           Hakyll
 import           Lucid.Base
 import           Lucid.Html5
@@ -18,7 +20,7 @@ entryListTemplate icons = LucidTemplate $ do
   main_ [class_ "list", role_ "main"] $ do
     toHtmlRaw body
 
-    div_ [class_ "container"] $
+    div_ [class_ "container"] $ do
       div_ [class_ "items"] $ do
         ListField ctx items <- lookupMeta "posts"
         forM_ (zip (repeat ctx) items) $ flip withContext $ do
@@ -30,3 +32,27 @@ entryListTemplate icons = LucidTemplate $ do
 
             div_ [class_ "post-contents"] $
               toHtmlRaw postBody
+
+      runLucidTemplate entryListNavigationTeplate
+
+entryListNavigationTeplate :: LucidTemplate a
+entryListNavigationTeplate = LucidTemplate $ do
+  hasPaginateContext <- isJust <$> lookupMetaMaybe "currentPageNum"
+  if not hasPaginateContext then return () else
+    nav_ [classes_ ["nav", "list-nav"]] $ do
+      div_ [class_ "prev"] $ do
+        mppu <- lookupMetaMaybe "previousPageUrl"
+        case mppu of
+             Just (StringField ppu) -> a_ [href_ (T.pack ppu)] "« Previous"
+             _                      -> " "
+
+      div_ [class_ "counter"] $ do
+        StringField numPages       <- lookupMeta "numPages"
+        StringField currentpageNum <- lookupMeta "currentPageNum"
+        toHtml $ "- " ++ currentpageNum ++ " of " ++ numPages ++ " -"
+
+      div_ [class_ "next"] $ do
+        mnpu <- lookupMetaMaybe "nextPageUrl"
+        case mnpu of
+             Just (StringField npu) -> a_ [href_ (T.pack npu)] "Next »"
+             _                      -> " "

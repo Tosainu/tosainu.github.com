a9548f077241fe0d0a69595b37f5eced3af39f31
Kenta Sato <tosainu.maple@gmail.com>
2018-08-12 23:10:55 +0900

    Use normalize.css instead of compass/reset

--- gulpfile.js
+++ gulpfile.js
@@ -20,12 +20,17 @@ function scss() {
       .pipe(gulp.dest('build/stylesheets/'));
 }
 
+function css() {
+  return gulp.src('node_modules/normalize.css/normalize.css')
+      .pipe(gulp.dest('build/stylesheets/'));
+}
+
 function other_files() {
   return gulp.src('src/{CNAME,favicon.ico}')
       .pipe(gulp.dest('build/'));
 }
 
-const build = gulp.parallel(html, scss, other_files);
+const build = gulp.parallel(html, scss, css, other_files);
 
 gulp.task('build', build);
 gulp.task('default', build);

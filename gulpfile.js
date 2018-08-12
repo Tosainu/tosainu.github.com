e93e17768afe741ef83fd42ee71d7313220a96ca
Kenta Sato <tosainu.maple@gmail.com>
2018-08-12 23:00:18 +0900

    Implement gulp task for style.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const embedsvg = require('gulp-embed-svg');
 const htmlmin = require('gulp-htmlmin');
+const sass = require('gulp-sass');
 
 function html() {
   return gulp.src('src/index.html')
@@ -11,12 +12,20 @@ function html() {
       .pipe(gulp.dest('build/'));
 }
 
+function scss() {
+  return gulp.src('src/stylesheets/*.scss')
+      .pipe(sass({
+        outputStyle: 'compressed'
+      }))
+      .pipe(gulp.dest('build/stylesheets/'));
+}
+
 function other_files() {
   return gulp.src('src/{CNAME,favicon.ico}')
       .pipe(gulp.dest('build/'));
 }
 
-const build = gulp.parallel(html, other_files);
+const build = gulp.parallel(html, scss, other_files);
 
 gulp.task('build', build);
 gulp.task('default', build);

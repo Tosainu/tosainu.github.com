c6c47a9bfe62e7bb88e0262a8355ea6d5d0948c7
Kenta Sato <tosainu.maple@gmail.com>
2018-08-13 01:04:03 +0900

    Font Awesome

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
 const gulp = require('gulp');
+const cheerio = require('gulp-cheerio');
 const embedsvg = require('gulp-embed-svg');
 const htmlmin = require('gulp-htmlmin');
+const rename = require("gulp-rename");
 const sass = require('gulp-sass');
+
+const fontawesome = require('@fortawesome/fontawesome-svg-core');
+fontawesome.library.add(require('@fortawesome/free-brands-svg-icons').fab);
+fontawesome.library.add(require('@fortawesome/free-solid-svg-icons').fas);
 
 function html() {
   return gulp.src('src/index.html')
       .pipe(embedsvg({
         root: 'src'
+      }))
+      .pipe(cheerio(function($, file) {
+        $('i.fab, i.fas').each(function() {
+          let i       = $(this);
+          let classes = i.attr('class').split(' ');
+          let prefix  = classes.shift();
+          let name    = classes.shift().replace(/^fa-/, '');
+          i.replaceWith(
+              fontawesome.icon({prefix: prefix, iconName: name}, {classes: classes}).html);
+        });
       }))
       .pipe(htmlmin({collapseWhitespace: true}))
       .pipe(gulp.dest('build/'));
@@ -21,7 +37,9 @@ function scss() {
 }
 
 function css() {
-  return gulp.src('node_modules/normalize.css/normalize.css')
+  return gulp.src('node_modules/@fortawesome/fontawesome-svg-core/styles.css')
+      .pipe(rename('fontawesome.css'))
+      .pipe(gulp.src('node_modules/normalize.css/normalize.css'))
       .pipe(gulp.dest('build/stylesheets/'));
 }
 

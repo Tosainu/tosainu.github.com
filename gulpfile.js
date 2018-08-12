45cc2b8a9fe15b45c38e213879d288048ccf60d2
Kenta Sato <tosainu.maple@gmail.com>
2018-08-12 22:39:43 +0900

    Implement gulp task for index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+
+function html() {
+  return gulp.src('src/index.html')
+      .pipe(htmlmin({collapseWhitespace: true}))
+      .pipe(gulp.dest('build/'));
+}
 
 function other_files() {
   return gulp.src('src/{CNAME,favicon.ico}')
       .pipe(gulp.dest('build/'));
 }
 
-const build = gulp.parallel(other_files);
+const build = gulp.parallel(html, other_files);
 
 gulp.task('build', build);
 gulp.task('default', build);

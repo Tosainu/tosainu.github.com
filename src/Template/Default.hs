de1e5ca1733c8b0c48af33b401e13df7e1a7465a
Kenta Sato <tosainu.maple@gmail.com>
2018-08-14 00:14:59 +0900

    Fix typo

--- src/Template/Default.hs
+++ src/Template/Default.hs
@@ -43,7 +43,7 @@ defaultTemplate = LucidTemplate $ do
   html_ [lang_ $ T.pack lang] $ do
     head_ $ do
       meta_ [charset_ "utf-8"]
-      meta_ [name_ "authr",       content_ (T.pack name)]
+      meta_ [name_ "author",       content_ (T.pack name)]
       meta_ [name_ "description", content_ (T.pack description)]
       meta_ [name_ "generator",   content_ "Hakyll"]
       meta_ [name_ "viewport",    content_ "width=device-width, initial-scale=1"]

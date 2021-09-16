b77e12cdce46dd508ee43ed1f6f18328344c040d
Kenta Sato <tosainu.maple@gmail.com>
2021-09-16 20:14:59 +0900

    update dependencies

--- tools/katex.js
+++ tools/katex.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const katex = require('katex');
 
-const input = fs.readFileSync(process.stdin.fd, 'utf-8');
+const input = fs.readFileSync(0, 'utf-8');
 const displayMode = process.argv.includes('displayMode');
 const result = katex.renderToString(input, { displayMode: displayMode });
 process.stdout.write(result);

24a5ee0716254844fed23eeedcd97bdccb56f02b
Kenta Sato <tosainu.maple@gmail.com>
2022-02-13 11:55:49 +0900

    update

--- src/Main.hs
+++ src/Main.hs
@@ -186,7 +186,7 @@ siteContext   = constField "lang"              "ja"
              <> constField "site-description"  "とさいぬのブログです"
              <> constField "site-url"          "https://blog.myon.info"
              <> constField "site-image"        "/images/icon/cocoa-512.jpg"
-             <> constField "copyright"         "© 2011-2020 Tosainu."
+             <> constField "copyright"         "© 2011-2022 Tosainu."
              <> constField "google-analytics"  "UA-57978655-1"
              <> constField "disqus"            "tosainu"
              <> authorContext

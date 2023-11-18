a10d8005b7337deae2ab0ff15458345ee1927d04
Kenta Sato <tosainu.maple@gmail.com>
2023-11-19 04:03:54 +0900

    make tsc happy

--- src/utils/applyPaginatePrefix.ts
+++ src/utils/applyPaginatePrefix.ts
@@ -1,44 +1,32 @@
-import type { GetStaticPathsResult, GetStaticPathsItem, Page } from 'astro'
+import type { Page, Params, Props } from 'astro'
 
-const applyPaginatePrefix = (
-  staticPaths: GetStaticPathsResult,
+const applyPaginatePrefix = <
+  PageData,
+  PaginateParams extends Params & { page: string | undefined },
+  PaginateProps extends Props & { page: Page<PageData> },
+>(
+  data: { params: PaginateParams; props: PaginateProps }[],
   prefix: string,
-): GetStaticPathsResult =>
-  staticPaths.map(({ params, props }): GetStaticPathsItem => {
-    const paramName = 'page'
-
-    const newParams = {
-      ...params,
-      [paramName]: params[paramName] === undefined ? undefined : prefix + params[paramName],
-    }
-
-    if (props === undefined || props[paramName] === undefined) {
-      return { params, props: undefined }
-    }
-
-    const page = props[paramName] as Page
-    const pageNum = page.currentPage
-    const url = page.url
-
-    const newProps = {
-      ...props,
-      [paramName]: {
-        ...page,
-        url: {
-          ...(url.current && {
-            current: url.current.replace(new RegExp(`/(${pageNum}/?)$`), `/${prefix}$1`),
-          }),
-          ...(url.next && {
-            next: url.next.replace(new RegExp(`/(${pageNum + 1}/?)$`), `/${prefix}$1`),
-          }),
-          ...(url.prev && {
-            prev: url.prev.replace(new RegExp(`/(${pageNum - 1}/?)$`), `/${prefix}$1`),
-          }),
+) =>
+  data.map(({ params, props }) => {
+    const { currentPage, url } = props.page
+    return {
+      params: {
+        ...params,
+        page: params.page === undefined ? undefined : prefix + params.page,
+      },
+      props: {
+        ...props,
+        page: {
+          ...props.page,
+          url: {
+            current: url.current.replace(new RegExp(`/(${currentPage}/?)$`), `/${prefix}$1`),
+            next: url.next?.replace(new RegExp(`/(${currentPage + 1}/?)$`), `/${prefix}$1`),
+            prev: url.prev?.replace(new RegExp(`/(${currentPage - 1}/?)$`), `/${prefix}$1`),
+          },
         },
       },
     }
-
-    return { params: newParams, props: newProps }
   })
 
 export default applyPaginatePrefix

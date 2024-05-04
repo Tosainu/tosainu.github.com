54d0a7a0de604a37efd17852c43b24ae781a5b22
Kenta Sato <tosainu.maple@gmail.com>
2024-05-04 16:55:28 +0900

    Bump eslint 9.2.0

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import astro from 'eslint-plugin-astro';
+import globals from 'globals';
+import js from '@eslint/js';
+import sortDestructureKeys from 'eslint-plugin-sort-destructure-keys';
+import typescript from 'typescript-eslint';
+
+export default typescript.config(
+  {
+    ignores: ['dist/**', '**/*.d.ts'],
+  },
+
+  js.configs.recommended,
+  ...typescript.configs.recommended,
+  ...astro.configs['flat/recommended'],
+
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2022,
+        ...globals.node,
+      },
+    },
+  },
+
+  {
+    plugins: {
+      ['sort-destructure-keys']: sortDestructureKeys,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'sort-destructure-keys/sort-destructure-keys': ['warn', { caseSensitive: false }],
+    },
+  },
+);
